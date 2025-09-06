6b46f8b319d3e221a583bb565e9622343b0b701d
Flow2510 <sendra.florian@gmail.com>
2025-09-06 14:54:01 +0200

    ajout de sr read pour les screen reader et du texte au label

--- src/assets/components/search/search.jsx
+++ src/assets/components/search/search.jsx
@@ -8,10 +8,12 @@ export default function Search({ filteredRegion, setFilteredRegion, setFilteredC
     return(
         <section className='search'>
             <label className='search__label search__label-search' htmlFor="search-input">
+                <span className="sr-only">Search country</span>
                 <i className="fa-solid fa-magnifying-glass"></i>
                 <input type="text" placeholder='Search for a country...' id='search-input' name='search-input' onChange={(e) => setFilteredCountry(e.target.value)}/>
             </label>
             <label className='search__label search__label-region' htmlFor="search-select">
+                <span className="sr-only">Choose region</span>
                 <select value={filteredRegion} className='search__select' name="search-select" id="search-select" onChange={(e) => setFilteredRegion(e.target.value)} >
                     <option value="">
                         Filter by Region
