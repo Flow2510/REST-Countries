3dc95812c5090db81deb969447af871d5d459067
Flow2510 <sendra.florian@gmail.com>
2025-09-06 14:57:46 +0200

    suppression des htmlfor

--- src/assets/components/search/search.jsx
+++ src/assets/components/search/search.jsx
@@ -7,12 +7,12 @@ export default function Search({ filteredRegion, setFilteredRegion, setFilteredC
 
     return(
         <section className='search'>
-            <label className='search__label search__label-search' htmlFor="search-input">
+            <label className='search__label search__label-search'>
                 <span className="sr-only">Search country</span>
                 <i className="fa-solid fa-magnifying-glass"></i>
                 <input type="text" placeholder='Search for a country...' id='search-input' name='search-input' onChange={(e) => setFilteredCountry(e.target.value)}/>
             </label>
-            <label className='search__label search__label-region' htmlFor="search-select">
+            <label className='search__label search__label-region'>
                 <span className="sr-only">Choose region</span>
                 <select value={filteredRegion} className='search__select' name="search-select" id="search-select" onChange={(e) => setFilteredRegion(e.target.value)} >
                     <option value="">
