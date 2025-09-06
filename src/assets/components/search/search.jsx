af371938d5485f44702572ef3da0d9f020ae1600
Flow2510 <sendra.florian@gmail.com>
2025-09-06 14:31:34 +0200

    ajout de name et id a input

--- src/assets/components/search/search.jsx
+++ src/assets/components/search/search.jsx
@@ -7,9 +7,9 @@ export default function Search({ filteredRegion, setFilteredRegion, setFilteredC
 
     return(
         <section className='search'>
-            <label className='search__label search__label-search' htmlFor="">
+            <label className='search__label search__label-search' htmlFor="search-input">
                 <i className="fa-solid fa-magnifying-glass"></i>
-                <input type="text" placeholder='Search for a country...' onChange={(e) => setFilteredCountry(e.target.value)}/>
+                <input type="text" placeholder='Search for a country...' id='search-input' name='search-input' onChange={(e) => setFilteredCountry(e.target.value)}/>
             </label>
             <label className='search__label search__label-region' htmlFor="search-select">
                 <select value={filteredRegion} className='search__select' name="search-select" id="search-select" onChange={(e) => setFilteredRegion(e.target.value)} >
