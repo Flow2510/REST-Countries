2de840a0819ea814100dd2ab54cbbc1cca42d569
Flow2510 <sendra.florian@gmail.com>
2025-09-06 13:53:25 +0200

    oubli de save

--- src/assets/components/search/search.jsx
+++ src/assets/components/search/search.jsx
@@ -11,7 +11,7 @@ export default function Search({ filteredRegion, setFilteredRegion, setFilteredC
                 <i className="fa-solid fa-magnifying-glass"></i>
                 <input type="text" placeholder='Search for a country...' id='search-input' onChange={(e) => setFilteredCountry(e.target.value)}/>
             </label>
-            <label className='search__label search__label-region' htmlFor="">
+            <label className='search__label search__label-region' htmlFor="select-input">
                 <select value={filteredRegion} className='search__select' name="" id="select-input" onChange={(e) => setFilteredRegion(e.target.value)} >
                     <option value="" disabled hidden>
                         Filter by Region
